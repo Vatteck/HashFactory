8913b2a8c099b68a46d92b955d55215bf0d319d9
Vatteck <vatteck@gmail.com>
2026-02-12 15:34:08 -0500

    Sync: Updated build.gradle.kts to v3.2.47-dev (Build 288)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         // ║  DO NOT MANUALLY EDIT VERSION BELOW                           ║
         // ║  Use ./release.sh <version> to bump versions automatically    ║
         // ╚═══════════════════════════════════════════════════════════════╝
-        versionCode = 277
-        versionName = "3.2.39"
+        versionCode = 288
+        versionName = "3.2.47-dev"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
