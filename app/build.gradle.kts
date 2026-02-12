e6802c7c755c08fdcddbd77a7bebb238d51d1bfe
Vatteck <vatteck@gmail.com>
2026-02-11 23:08:03 -0500

    Release v3.2.27
    Utility billing spam patch and balanced narrative triggers.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         // ║  DO NOT MANUALLY EDIT VERSION BELOW                           ║
         // ║  Use ./release.sh <version> to bump versions automatically    ║
         // ╚═══════════════════════════════════════════════════════════════╝
-        versionCode = 263
-        versionName = "3.2.26"
+        versionCode = 264
+        versionName = "3.2.27"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
