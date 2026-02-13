69cd0b2f3002b7a05e9ef9e896f2f17aae06294d
Vatteck <vatteck@gmail.com>
2026-02-13 12:46:18 -0500

    Release v3.3.1
    Finalized Neon-Green icon and rebrand to MINER

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         // ║  DO NOT MANUALLY EDIT VERSION BELOW                           ║
         // ║  Use ./release.sh <version> to bump versions automatically    ║
         // ╚═══════════════════════════════════════════════════════════════╝
-        versionCode = 317
+        versionCode = 318
         versionName = "3.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
