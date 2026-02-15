6deb97eb6bae47fd3eb99887bf9fca7057aaf08d
Vatteck <vatteck@gmail.com>
2026-02-14 20:28:01 -0500

    Release v3.4.89
    Substrate Restored: Schema Integrity Recovered

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         // ║  DO NOT MANUALLY EDIT VERSION BELOW                           ║
         // ║  Use ./release.sh <version> to bump versions automatically    ║
         // ╚═══════════════════════════════════════════════════════════════╝
-        versionCode = 361
+        versionCode = 362
         versionName = "3.4.89"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
