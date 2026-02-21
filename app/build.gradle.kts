87832482bd6efa422c6a60959d50acdd734c496e
Vatteck <vatteck@gmail.com>
2026-02-21 16:04:21 -0500

    Release v3.12.4
    Auto-release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         // ║  DO NOT MANUALLY EDIT VERSION BELOW                           ║
         // ║  Use ./release.sh <version> to bump versions automatically    ║
         // ╚═══════════════════════════════════════════════════════════════╝
-        versionCode = 414
-        versionName = "3.12.3"
+        versionCode = 415
+        versionName = "3.12.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
